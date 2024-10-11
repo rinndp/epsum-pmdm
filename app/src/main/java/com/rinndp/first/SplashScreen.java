b4c5d5fa52dcf1c96e5d9f3f9a215a97a6cca2a6
axel <axelperez221105@gmail.com>
2024-10-11 13:12:32 +0200

    Actualizado con mario

--- app/src/main/java/com/rinndp/first/SplashScreen.java
+++ app/src/main/java/com/rinndp/first/SplashScreen.java
@@ -1,12 +1,19 @@
 package com.rinndp.first;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -20,5 +27,19 @@ public class SplashScreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Animation gradientColour = AnimationUtils.loadAnimation(this, R.anim.gradient_colour);
+        ImageView splashLogo = findViewById(R.id.splashLogo);
+        TextView splashAppAnme = findViewById(R.id.splashAppName);
+        splashLogo.startAnimation(gradientColour);
+        splashAppAnme.startAnimation(gradientColour);
+
+        ImageView glideBackground = findViewById(R.id.splashGlideBackground);
+
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1665162848578-6fefca8e76d1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+                .transition(DrawableTransitionOptions.withCrossFade(1000))
+                .centerCrop()
+                .into(glideBackground);
     }
 }
